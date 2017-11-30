18d14616c95487bbbe82c4300c99010380d59913
Nihar Sheth <nsheth12@gmail.com>
2017-11-30 13:16:34 -0800

    Kian's updates

--- match_transcript.py
+++ match_transcript.py
@@ -1,16 +1,8 @@
 import os, re
 
-kelly_path = "../kelly_qtype_csvs/"
-audio_path = "../mono8khz/"
 
-
-kelly_names = os.listdir(kelly_path)
-kelly_ids = [re.split("_|-", f)[0] for f in kelly_names]
-
-audio_names = os.listdir(audio_path)
-audio_ids = [re.split("_|-", f)[0] for f in audio_names]
-
-result = {}
+def get_ids(names):
+	return [re.split("_|-", f)[0] for f in names]
 
 def clean_audio(audio_names, audio_ids):
 	audio_names.sort()
@@ -28,14 +20,60 @@ def clean_audio(audio_names, audio_ids):
 		else:
 			i += 1
 
-clean_audio(audio_names, audio_ids)					
+def clean_ids(ids):
+	ids = ids
+	return [ re.sub("\D", "", bad_id) for bad_id in ids ]
+
+
+kelly_path = "../kelly_qtype_csvs/"
+back_up_path = "../forensic_interview_transcripts/"
+audio_path = "../mono8khz/"
+
+"""
+The folders within the forensic_interview_transcripts directory
+did not have any files that were not already in the forensic_interview_transcripts
+directory, so we removed them from the search path
+"""
+
+
+kelly_names = os.listdir(kelly_path)
+kelly_ids = get_ids(kelly_names)
+
+backup_names = os.listdir(back_up_path)
+backup_ids = get_ids(backup_names)
+clean_back_up_ids = clean_ids(backup_ids)
+
+print(clean_back_up_ids)
+
+#last_resort_names = os.listdir(last_resort_path)
+#last_resort_ids = get_ids(last_resort_names)
+##print(len(backup_ids))
+
+audio_names = os.listdir(audio_path)
+audio_ids = [re.split("_|-", f)[0] for f in audio_names]
+
+result = {}
+	
+
+clean_audio(audio_names, audio_ids)
+					
 
 for audio_index, audio_id in enumerate(audio_ids):
 	try:
 		pos = kelly_ids.index(audio_id)
 		result[audio_names[audio_index]] = kelly_names[pos]
 	except:
-		print("No transcript for ", audio_id)
+		try:
+			pos = backup_ids.index(audio_id)
+			result[audio_names[audio_index]] = backup_names[pos]
+
+		except:	
+			try:
+				pos = clean_back_up_ids.index(audio_id)
+			except:
+				
+				print("No transcript for " + audio_id)
+			
 
 print(len(result))
 
