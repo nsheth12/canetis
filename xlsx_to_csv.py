58dc4c891f027cbf45b40095c11df3c0af907952
Nihar Sheth <nsheth12@gmail.com>
2017-12-01 17:26:51 -0800

    Not sure what happened with match_transcript, will try to figure out

---
+++ xlsx_to_csv.py
@@ -0,0 +1,41 @@
+import os, subprocess, sys
+
+
+def get_file_names(back_up_path):
+	return os.listdir(back_up_path)
+
+
+def get_csv(xlsx):
+	csv = xlsx
+	csv = csv.split(".")
+	csv[-1] = "csv"
+	csv = ".".join(csv)
+	return csv
+
+
+
+	
+
+
+
+
+
+xlsx_path = sys.argv[1] 
+end_path = sys.argv[2]
+
+
+
+
+
+xlsx_files = get_file_names(xlsx_path)
+
+	
+
+for xlsx in xlsx_files:
+	xlsx_path = xlsx_path+"/"+ xlsx	
+	csv_path = end_path+"/" +get_csv(xlsx)
+	subprocess.call(["touch", csv_path])
+	subprocess.call(["ssconvert", xlsx_path, csv_path])
+
+
+	
