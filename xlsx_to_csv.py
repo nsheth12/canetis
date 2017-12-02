4113e004b5a0f6ab72f79504ded7c6bc2313bcf8
Nihar Sheth <nsheth12@gmail.com>
2017-12-01 18:28:30 -0800

    updated xlsx to csv converter

--- xlsx_to_csv.py
+++ xlsx_to_csv.py
@@ -13,26 +13,19 @@ def get_csv(xlsx):
 	return csv
 
 
-
-	
-
-
-
-
-
 xlsx_path = sys.argv[1] 
 end_path = sys.argv[2]
 
 
 
-
-
 xlsx_files = get_file_names(xlsx_path)
+
 
 	
 
 for xlsx in xlsx_files:
 	xlsx_path = xlsx_path+"/"+ xlsx	
+	print(xlsx_path)
 	csv_path = end_path+"/" +get_csv(xlsx)
 	subprocess.call(["touch", csv_path])
 	subprocess.call(["ssconvert", xlsx_path, csv_path])
