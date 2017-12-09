53ab66a7fdab25d41ac8a0bd434604fa2586b665
Nihar Sheth <nsheth12@gmail.com>
2017-12-08 21:39:12 -0800

    Added utils

---
+++ utils.py
@@ -0,0 +1,16 @@
+import Segment
+
+def gentle (s):
+	"""
+	takes in a segment
+	1. create new text file containing text
+	2. create new audio with librosa
+	3. run Gentle with these two
+	4. delete text file/audio files
+	"""
+
+def segmentize (d):
+	"""
+	takes in Gentle output (array of dicts)
+	break into segments which marked as aligned or unaligned
+	"""
