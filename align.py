8e56b60adc4b6595b0dada6237ba880a82ae6c1d
Nihar Sheth <nsheth12@gmail.com>
2017-12-08 21:58:13 -0800

    Added more psuedocode for main aligner

--- align.py
+++ align.py
@@ -5,19 +5,17 @@ import librosa
 
 
 def align(audio_file, text_file, error_rate=.05):
-	
-	#get audio andr text files
-	
-	#run gentle and get output dictionary
-
-	
-	#run recursion and set output equal to result array
-
-	
-	#return result array 
+	# get audio and text files
+	# run gentle and get output dictionary
+	# run recursion and set output equal to result array
+	# return result array 
 
 
-def recurse(gentle_output, audio_file_maybe_path):
-	#segmentize the dictionary and store list of segments
-
-	#
+def recurse(gentle_output, audio_file_maybe_path, error_rate):
+	# if % unaligned in gentle_output < 5%, return gentle output
+	# call segmentize on gentle_output, getting list of segments
+	res = []
+	# loop through each segment
+	#	if aligned --> add to res as is
+	#	else add recurse(Gentle(segment))
+	return res
