c193467912d27d01ad4fd72a315217fe9f83af8b
Kian Ghodoussi <ghodouss@usc.edu>
2017-12-08 21:47:47 -0800

    added main file

---
+++ align.py
@@ -0,0 +1,23 @@
+from segment import Segment
+from utils import *
+import librosa
+
+
+
+def align(audio_file, text_file, error_rate=.05):
+	
+	#get audio andr text files
+	
+	#run gentle and get output dictionary
+
+	
+	#run recursion and set output equal to result array
+
+	
+	#return result array 
+
+
+def recurse(gentle_output, audio_file_maybe_path):
+	#segmentize the dictionary and store list of segments
+
+	#
