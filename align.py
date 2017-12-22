f5b5433c8c017fa652d487b6e6200d2e1948f582
Nihar Sheth <nsheth12@gmail.com>
2017-12-22 00:37:56 -0500

    Added some more to recurse()

--- align.py
+++ align.py
@@ -1,4 +1,3 @@
-from segment import Segment
 from utils import gentle, segmentize
 
 def align(audio_file, text_file, error_rate=.05):
@@ -12,14 +11,19 @@ def recurse(gentle_output, audio_file_maybe_path, error_rate):
 	# if % unaligned in gentle_output < 5%, return gentle output
 	# call segmentize on gentle_output, getting list of segments
 	segs = segmentize(gentle_output)
+
+	if len(segs) == 1:
+		res.append(segs)
+		return res
+
 	res = []
 	# loop through each segment
 	for seg in segs:
-		# if aligned --> add to res as is
 		if seg.aligned:
+			# if aligned --> add to res as is
 			res.append(seg)
-		# else add recurse(Gentle(segment))
 		else:
+			# else add recurse(Gentle(segment))
 			res.append(recurse(gentle(seg)))
-			
+
 	return res
