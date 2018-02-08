7397b1ef97892217830d9fc24db8b5a8d3cbce17
Nihar Sheth <nsheth12@gmail.com>
2018-02-07 23:27:33 -0800

    Initial install script

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# set -e
+
+if ! command -v python > /dev/null; then
+    echo "Please install Python 2 and then run this script... exiting"
+    exit 1
+fi
+
+if ! command -v pip > /dev/null; then
+    echo "Please install pip for Python 2 and then run this script... exiting"
+    exit 1
+fi
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    apt-get install git
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    if command -v brew > /dev/null; then
+        brew install git
+    else
+        echo >&2 "Please install homebrew and then run this install script... exiting"
+        exit 1
+    fi
+fi
+
+pip install pydub
+git clone https://github.com/lowerquality/gentle.git
+rm -rf gentle/.git
+gentle/install.sh
+echo "export PYTHONPATH=gentle:${PYTHONPATH}" >> ~/.bashrc
+echo "export PYTHONPATH=gentle/gentle:${PYTHONPATH}" >> ~/.bashrc
