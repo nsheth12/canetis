c88522e3044179f2b3fd82c0674e7e52fe155900
Nihar Sheth <nsheth12@gmail.com>
2018-03-04 00:32:56 -0800

    update test file

--- test/test.py
+++ test/test.py
@@ -3,6 +3,8 @@ import os
 import json
 from align import align
 
+DIR_PATH = os.path.dirname(os.path.abspath(__file__))
+
 
 def process_output(basename):
     """Process Canetis and Gentle outputs for given file for concise output"""
@@ -12,7 +14,7 @@ def process_output(basename):
     total_words_gentle = 0
 
     # Canetis part
-    with open(os.path.join("canetis_results", basename)) as canetis_file:
+    with open(os.path.join(DIR_PATH, "canetis_results", basename)) as canetis_file:
         canetis_output = json.load(canetis_file)
 
         for word in canetis_output:
@@ -21,7 +23,7 @@ def process_output(basename):
             total_words_canetis += 1
 
     # Gentle part
-    with open(os.path.join("gentle_results", basename)) as gentle_file:
+    with open(os.path.join(DIR_PATH, "gentle_results", basename)) as gentle_file:
         gentle_output = json.load(gentle_file)
 
         for word in gentle_output:
@@ -36,11 +38,11 @@ def process_output(basename):
     print
 
 
-for audio_name in os.listdir("audio"):
+for audio_name in os.listdir(os.path.join(DIR_PATH, "audio")):
     transcript_name = os.path.splitext(audio_name)[0] + ".trn"
 
-    audio_path = os.path.join("audio", audio_name)
-    transcript_path = os.path.join("transcripts", transcript_name)
+    audio_path = os.path.join(DIR_PATH, "audio", audio_name)
+    transcript_path = os.path.join(DIR_PATH, "transcripts", transcript_name)
 
     if not os.path.isfile(transcript_path):
         continue
@@ -58,7 +60,7 @@ for audio_name in os.listdir("audio"):
         result = aligner.transcribe(wavfile).words
 
     # write Gentle output to gentle_results directory
-    with open(os.path.join("gentle_results", transcript_name + ".txt"), "w") as f:
+    with open(os.path.join(DIR_PATH, "gentle_results", transcript_name + ".txt"), "w") as f:
         output = []
         for word in result:
             output.append({"word": word.word, "success": word.success(),
@@ -70,7 +72,7 @@ for audio_name in os.listdir("audio"):
     canetis_output = align(audio_path, transcript_path)
 
     # write Canetis output to canetis_results directory
-    with open(os.path.join("canetis_results", transcript_name + ".txt"), "w") as f:
+    with open(os.path.join(DIR_PATH, "canetis_results", transcript_name + ".txt"), "w") as f:
         json.dump(canetis_output, f)
 
     # process and print outputs
