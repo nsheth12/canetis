d6657cfe04d15023d6d7c57feda0868261a34ce5
Nihar Sheth <nsheth12@gmail.com>
2018-03-04 16:13:11 -0800

    use very small test file to debug build failure

--- test/test.py
+++ test/test.py
@@ -53,11 +53,16 @@ for audio_name in os.listdir(os.path.join(DIR_PATH, "audio")):
         transcript_text = f.read()
 
     # run Gentle
-    print("About to run Gentle on " + transcript_name)
+    print "Running Gentle on", transcript_name
     resources = gentle.Resources()
     with gentle.resampled(audio_path) as wavfile:
         aligner = gentle.ForcedAligner(resources, transcript_text)
         result = aligner.transcribe(wavfile).words
+
+    # create gentle_results directory if it doesn't already exist
+    # usually better to use try-catch here, but not worried about race conditions right now
+    if not os.path.exists(os.path.join(DIR_PATH, "gentle_results")):
+        os.makedirs(os.path.join(DIR_PATH, "gentle_results"))
 
     # write Gentle output to gentle_results directory
     with open(os.path.join(DIR_PATH, "gentle_results", transcript_name + ".txt"), "w") as f:
@@ -68,8 +73,12 @@ for audio_name in os.listdir(os.path.join(DIR_PATH, "audio")):
         json.dump(output, f)
 
     # run Canetis
-    print("About to run Canetis on " + transcript_name)
+    print "Running Canetis on", transcript_name
     canetis_output = align(audio_path, transcript_path)
+
+    # create canetis_results directory if it doesn't already exist
+    if not os.path.exists(os.path.join(DIR_PATH, "canetis_results")):
+        os.makedirs(os.path.join(DIR_PATH, "canetis_results"))
 
     # write Canetis output to canetis_results directory
     with open(os.path.join(DIR_PATH, "canetis_results", transcript_name + ".txt"), "w") as f:
