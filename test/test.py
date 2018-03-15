5163a36fe3fff88ca934ad8240313d098ff1fa77
Nihar Sheth <nsheth12@gmail.com>
2018-03-15 01:00:12 -0400

    separate canetis and gentle build jobs

--- test/test.py
+++ test/test.py
@@ -6,47 +6,7 @@ from align import align
 DIR_PATH = os.path.dirname(os.path.abspath(__file__))
 
 
-def process_output(basename):
-    """Process Canetis and Gentle outputs for given file for concise output"""
-    successful_words_canetis = 0
-    total_words_canetis = 0
-    successful_words_gentle = 0
-    total_words_gentle = 0
-
-    # Canetis part
-    with open(os.path.join(DIR_PATH, "canetis_results", basename)) as canetis_file:
-        canetis_output = json.load(canetis_file)
-
-        for word in canetis_output:
-            if word["success"]:
-                successful_words_canetis += 1
-            total_words_canetis += 1
-
-    # Gentle part
-    with open(os.path.join(DIR_PATH, "gentle_results", basename)) as gentle_file:
-        gentle_output = json.load(gentle_file)
-
-        for word in gentle_output:
-            if word["success"]:
-                successful_words_gentle += 1
-            total_words_gentle += 1
-
-    print "Gentle successful:", successful_words_gentle
-    print "Canetis successful:", successful_words_canetis
-    print "Gentle total:", total_words_gentle
-    print "Canetis total:", total_words_canetis
-    print
-
-
-for audio_name in os.listdir(os.path.join(DIR_PATH, "audio")):
-    transcript_name = os.path.splitext(audio_name)[0] + ".trn"
-
-    audio_path = os.path.join(DIR_PATH, "audio", audio_name)
-    transcript_path = os.path.join(DIR_PATH, "transcripts", transcript_name)
-
-    if not os.path.isfile(transcript_path):
-        continue
-
+def test_gentle(audio_path, transcript_path, transcript_name):
     # get transcript text
     transcript_text = ""
     with open(transcript_path) as f:
@@ -72,6 +32,23 @@ for audio_name in os.listdir(os.path.join(DIR_PATH, "audio")):
                            "end": word.end, "start": word.start})
         json.dump(output, f)
 
+    successful_words_gentle = 0
+    total_words_gentle = 0
+
+    with open(os.path.join(DIR_PATH, "gentle_results", transcript_name + ".txt")) as gentle_file:
+        gentle_output = json.load(gentle_file)
+
+        for word in gentle_output:
+            if word["success"]:
+                successful_words_gentle += 1
+            total_words_gentle += 1
+
+    print "Gentle successful:", successful_words_gentle
+    print "Gentle total:", total_words_gentle
+    print
+
+
+def test_canetis(audio_path, transcript_path, transcript_name):
     # run Canetis
     print "Running Canetis on", transcript_name
     canetis_output = align(audio_path, transcript_path)
@@ -84,5 +61,35 @@ for audio_name in os.listdir(os.path.join(DIR_PATH, "audio")):
     with open(os.path.join(DIR_PATH, "canetis_results", transcript_name + ".txt"), "w") as f:
         json.dump(canetis_output, f)
 
-    # process and print outputs
-    process_output(transcript_name + ".txt")
+    successful_words_canetis = 0
+    total_words_canetis = 0
+
+    with open(os.path.join(DIR_PATH, "canetis_results", transcript_name + ".txt")) as canetis_file:
+        canetis_output = json.load(canetis_file)
+
+        for word in canetis_output:
+            if word["success"]:
+                successful_words_canetis += 1
+            total_words_canetis += 1
+
+    print "Canetis successful:", successful_words_canetis
+    print "Canetis total:", total_words_canetis
+    print
+
+
+for audio_name in os.listdir(os.path.join(DIR_PATH, "audio")):
+    transcript_name = os.path.splitext(audio_name)[0] + ".trn"
+
+    audio_path = os.path.join(DIR_PATH, "audio", audio_name)
+    transcript_path = os.path.join(DIR_PATH, "transcripts", transcript_name)
+
+    if not os.path.isfile(transcript_path):
+        continue
+
+    if "MODE" not in os.environ:
+        test_gentle(audio_path, transcript_path, transcript_name)
+        test_canetis(audio_path, transcript_path, transcript_name)
+    elif os.environ["MODE"] == "gentle":
+        test_gentle(audio_path, transcript_path, transcript_name)
+    else:
+        test_canetis(audio_path, transcript_path, transcript_name)
