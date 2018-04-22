567a5cf25503a486f09696fee7d53a3748a47306
Nihar Sheth <nsheth12@gmail.com>
2018-04-21 22:57:48 -0700

    new install script

--- install.sh
+++ install.sh
@@ -17,16 +17,26 @@ if ! command -v git > /dev/null; then
     exit 1
 fi
 
+# initialize Gentle submodule
+git submodule init
+git submodule update
+
+# initialize Kaldi submodule inside of Gentle
+(cd gentle && git submodule init && git submodule update)
+
+# install dependencies and Kaldi
+(cd gentle && ./install_deps.sh && cd ext && sudo ./install_kaldi.sh)
+
+# load models
+(cd gentle && sudo ./install_models.sh && cd ext && make depend && make)
+
 pip install pydub
-rm -rf .git
-git clone https://github.com/lowerquality/gentle.git
-(cd gentle && ./install.sh)
 
 # deal with Ubuntu 14.04 ffmpeg issues
 if ! command -v ffmpeg > /dev/null && [[ "$OSTYPE" == "linux-gnu" ]]; then
-    add-apt-repository -y ppa:mc3man/trusty-media
-    apt-get -y update
-    apt-get -y install ffmpeg
+    sudo add-apt-repository -y ppa:mc3man/trusty-media
+    sudo apt-get -y update
+    sudo apt-get -y install ffmpeg
 fi
 
 # taken from https://stackoverflow.com/a/246128
