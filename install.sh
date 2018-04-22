9dfa7c3d5396dde6886b96b41d9291c9ff28ff51
Nihar Sheth <nsheth12@gmail.com>
2018-04-22 00:50:40 -0700

    move install_deps into canetis install script

--- install.sh
+++ install.sh
@@ -24,8 +24,28 @@ git submodule update
 # initialize Kaldi submodule inside of Gentle
 (cd gentle && git submodule init && git submodule update)
 
+# ripped from Gentle install script
+###################################
+echo "Installing dependencies..."
+
+# Install OS-specific dependencies
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+	sudo apt-get update -qq
+	sudo apt-get install -y zlib1g-dev automake autoconf git \
+		libtool subversion libatlas3-base python-pip \
+		python-dev wget unzip
+	sudo apt-get install -y ffmpeg || echo -n  "\n\nYou have to install ffmpeg from a PPA or from https://ffmpeg.org before you can run gentle\n\n"
+	pip install .
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	brew install ffmpeg libtool automake autoconf wget
+
+	sudo easy_install pip
+	sudo pip install .
+fi
+###################################
+
 # install dependencies and Kaldi
-(cd gentle && ./install_deps.sh && cd ext && sudo ./install_kaldi.sh)
+(cd gentle/ext && sudo ./install_kaldi.sh)
 
 # load models
 (cd gentle && sudo ./install_models.sh && cd ext && make depend && make)
